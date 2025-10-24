f35b53c41f3528e8531930e9eab9a20d8a97e62a
andkay <andkay@users.noreply.github.com>
2025-10-24 18:56:33 +0000

    deploy: 34539d0619c957d54220151512fa5d2e05266212

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1000.dev3480+g00a76ef4d',
+    VERSION: '1000.dev3481+g34539d061',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
